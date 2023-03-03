da6c0944d234918e51f2b7016615d17fc2c316f1
Isaac Mendoza <isaacmsd9@gmail.com>
2023-03-03 14:43:38 +0000

    cifrado

--- Ejemplo_lab/ejemplolab7.c
+++ Ejemplo_lab/ejemplolab7.c
@@ -4,19 +4,24 @@
 Se recorre un arreglo bidimensional a través de un apuntador
  */
 
- int main(){
+ int main()
+ {
     
     int *ap, indice;
     int nums[3][3] = {{99,88,77},{66,55,44},{33,22,11}};
     ap = nums;
     
-    for (indice = 0; indice < 9 ; indice++){
+    for (indice = 0; indice < 9 ; indice++)
+    {
         if ((indice%3)==0)
         {
             printf("\n");
         }
+        
+        printf("%x\t",(ap+indice));
+    }
     
-    printf("%x\t",(ap+indice));
+    
     
     return 0;
  }
