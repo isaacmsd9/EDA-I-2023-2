aaaa373141956a3706723853538949782fc3aabe
Isaac Mendoza <isaacmsd9@gmail.com>
2023-05-05 14:19:55 +0000

    f

---
+++ Ejemplo_lab/Semana09/ejemplo1.py
@@ -0,0 +1,8 @@
+
+print("Hola mundo")
+#comentario corto
+"""comentario largo"""
+
+#puedes poner una función como argumento de una función
+#puedes imprimir una función o operaciones sin poner"%"
+#
