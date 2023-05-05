97e335641d8494a97f5b2c2d8a0bb2679d226bd9
Isaac Mendoza <isaacmsd9@gmail.com>
2023-05-05 14:26:35 +0000

    f

--- Ejemplo_lab/Semana09/ejemplo1.py
+++ Ejemplo_lab/Semana09/ejemplo1.py
@@ -5,4 +5,11 @@ print("Hola mundo")
 
 #puedes poner una función como argumento de una función
 #puedes imprimir una función o operaciones sin poner"%"
-#
+#concatenar cadenas con +
+#casting un tipo de dato representar otro tipo de dato variable
+#variable de cualquier tipo=input("mensaje")
+#print(int(variableAntesflotante))
+#cell() redondea arriba pero necesita una variable
+#floor() redondea para abajo pero necesita una variable
+#round() redondea no necesita una variable
+#abs() es el valor absoluto quita signos
