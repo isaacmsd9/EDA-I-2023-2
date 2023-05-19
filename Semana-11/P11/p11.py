a511245a900a3651f3df44851c9c0d221c6b6aa1
Isaac Mendoza <isaacmsd9@gmail.com>
2023-05-19 03:11:50 +0000

    f

--- Semana-11/P11/p11.py
+++ Semana-11/P11/p11.py
@@ -2,7 +2,7 @@ def decimal_a_binario(n):
     if n == 0:
         return ''
     else:
-        return decimal_a_binario(n//2) + str(n%2)
+        return decimal_a_binario(n // 2) + str(n % 2)
 
 def fibonacci_inverso(n):
     if n == 0:
@@ -12,22 +12,22 @@ def fibonacci_inverso(n):
     elif n == 2:
         return [1, 0]
     else:
-        fib = fibonacci_inverso(n-1)
+        fib = fibonacci_inverso(n - 1)
         fib.append(fib[-1] + fib[-2])
         return fib
 
 def menu():
-    print('Bienvenido, puede realizar las siguientes operaciones:')
+    print('Bienvenido, puede realizar las siguientes operaciones:\n')
     print('1 Conversion de bases')
     print('2 Serie de Fibonacci')
-    print('3 Salir')
+    print('3 Salir\n')
     opcion = int(input('Ingrese la opcion: '))
     if opcion == 1:
-        n = int(input('Ingrese un número entero: '))
-        print(decimal_a_binario(n))
+        n = int(input('\nIngrese un número entero: '))
+        print('\n' + decimal_a_binario(n))
     elif opcion == 2:
-        n = int(input('Ingrese un número entero: '))
-        print(fibonacci_inverso(n)[::-1])
+        n = int(input('\nIngrese un número entero: '))
+        print('\n' + str(fibonacci_inverso(n)[::-1]))
     elif opcion == 3:
         exit()
 
