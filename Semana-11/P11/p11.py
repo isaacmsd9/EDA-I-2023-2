e8697642507476c84a62ea520d6e9969d6c16f66
Isaac Mendoza <isaacmsd9@gmail.com>
2023-05-19 01:46:58 +0000

    f

---
+++ Semana-11/P11/p11.py
@@ -0,0 +1,38 @@
+def decimal_a_binario(n):
+    if n == 0:
+        return ''
+    else:
+        return decimal_a_binario(n//2) + str(n%2)
+
+def fibonacci_inverso(n):
+    if n == 0:
+        return []
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [1, 0]
+    else:
+        fib = fibonacci_inverso(n-1)
+        fib.append(fib[-1] + fib[-2])
+        return fib
+
+def menu():
+    print('Bienvenido, puede realizar las siguientes operaciones:')
+    print('1 Conversion de bases')
+    print('2 Serie de Fibonacci')
+    print('3 Salir')
+    opcion = int(input('Ingrese la opcion: '))
+    if opcion == 1:
+        n = int(input('Ingrese un número entero: '))
+        print(decimal_a_binario(n))
+    elif opcion == 2:
+        n = int(input('Ingrese un número entero: '))
+        print(fibonacci_inverso(n)[::-1])
+    elif opcion == 3:
+        exit()
+
+def main():
+    menu()
+
+if __name__ == '__main__':
+    main()
