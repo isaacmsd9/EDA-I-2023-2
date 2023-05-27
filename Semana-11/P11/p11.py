5a35932068584aa129cd796310d4a8b16bc660a9
Isaac Mendoza <isaacmsd9@gmail.com>
2023-05-27 01:05:46 +0000

    f

--- Semana-11/P11/p11.py
+++ Semana-11/P11/p11.py
@@ -16,12 +16,9 @@ def decimal_a_binario(n):
         return decimal_a_binario(n // 2) + str(n % 2)
 
 def fibonacci(n):
-    """
-    Devuelve los primeros N términos de la serie de Fibonacci usando recursión.
-
+    """ Devuelve los primeros N términos de la serie de Fibonacci usando iteración.
     Args:
         n (int): El número de términos a devolver.
-
     Returns:
         list: Una lista con los primeros N términos de la serie de Fibonacci.
     """
@@ -35,17 +32,35 @@ def fibonacci(n):
         # Caso base: si n es 2, se devuelve una lista con los dos primeros términos de la serie
         return [0, 1]
     else:
-        # Caso recursivo: se obtienen los primeros n-1 términos y se agrega el siguiente término al final
-        fib = fibonacci(n - 1)
-        fib.insert(fib[-1] + fib[-2])
+        # Caso iterativo: se calculan los términos de la serie uno por uno
+        fib = [0, 1]
+        for i in range(2, n):
+            fib.append(fib[-1] + fib[-2])
         return fib
+
+def swap(lista, menor, mayor): 
+    temp = lista[menor] 
+    lista[menor]= lista[mayor]
+    lista[mayor]= temp
+
+def selectionSort(list, n): 
+
+    for i in range (0, n-1): 
+        maximum = i
+        for j in range (i+1, n):
+            if list[j] > list[maximum]: 
+                maximum = j
+
+        if maximum != i:
+            swap(list, maximum, i) 
+    return list 
 
 if __name__ == '__main__':
     # Este bloque de código solo se ejecuta si el archivo se ejecuta como un script
     while True:
         # Muestra un menú de opciones al usuario
         print('\n\033[1;36mBienvenido, puede realizar las siguientes operaciones:\033[0m\n')
-        print('\033[1;33m1 Conversion de bases\033[0m\n')
+        print('\033[1;33m1 Conversión de bases\033[0m\n')
         print('\033[1;33m2 Serie de Fibonacci\033[0m\n')
         print('\033[1;33m3 Salir\033[0m\n')
         
@@ -60,7 +75,7 @@ if __name__ == '__main__':
         elif opcion == 2:
             # Si el usuario eligió la opción 2, se pide un número entero y se muestran los primeros N términos de la serie de Fibonacci
             n = int(input('\n\033[1;34mIngrese el número entero N: \033[0m'))
-            print('\n\033[1;32m' + ' '.join(map(str, fibonacci(n)[::-1])) + '\033[0m')
+            print('\n\033[1;32m' + ' '.join(map(str, selectionSort(fibonacci(n), n))) + '\033[0m') #selectionSort() trabaja con lo que devuelva fibonacci(), ambas regresan una cadena, solo que lo que imprimir print es la que regresa selectionSort
             
         elif opcion == 3:
             # Si el usuario eligió la opción 3, se termina el programa
